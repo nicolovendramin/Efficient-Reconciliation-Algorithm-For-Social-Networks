2c5d98590bab647dd7e54e609f2d69d691d174ad
Nicolò Vendramin <nicolovendramin@Nicolos-MacBook-Pro.local>
2017-05-30 18:25:10 +0200

    New part added

---
+++ code/test_main.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import NetworkGenerator as ng
+import ReconciliationAlgo as ra
+
+
+generator = ng.GetGenerator()
+generator.generate(1000, 10, 0.4, 0.5, 0.4)
+g1, g2, L = generator.get_realizations()
+D = generator.get_maxdegree()
+l = len(L)
+r = ra.get_reconciliator()
+L = r.reconcile_naive(g1, g2, L, 2, D, 2)
+
+print(len(L), l)
+
